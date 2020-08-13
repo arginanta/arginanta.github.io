2d5cc9e0379517e233abec9b683bc8b598893581
arginanta <arginanta30@gmail.com>
2020-08-13 16:57:05 +0700

    Menyalin isi folder build

---
+++ precache-manifest.82d3934a8ac57a07e72f6b709b159f1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9313fcc75e03e865d4f2c346dd22f4d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "323b79e5e2d239ab891d",
+    "url": "/static/css/main.b16ef6e8.chunk.css"
+  },
+  {
+    "revision": "9b19829c01ff4298e12d",
+    "url": "/static/js/2.b93a9309.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b93a9309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323b79e5e2d239ab891d",
+    "url": "/static/js/main.3be9b889.chunk.js"
+  },
+  {
+    "revision": "28648d4b86d330626cee",
+    "url": "/static/js/runtime-main.fafa68b7.js"
+  },
+  {
+    "revision": "9add47a9afa2febbb4a406635cf6bc91",
+    "url": "/static/media/aneka-makanan.9add47a9.png"
+  }
+]);
